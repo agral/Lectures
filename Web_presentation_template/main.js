a7825822710cb07c96e310e134662fdef593a734
agral <adam@gralin.ski>
2025-11-03 22:49:07 +0100

    Finish up making of footer entirely in main.js

--- Web_presentation_template/main.js
+++ Web_presentation_template/main.js
@@ -6,12 +6,29 @@ function CreateFooter(root, slideNum, totalSlides) {
     const footer = document.createElement("footer")
     const divLeft = document.createElement("div")
     divLeft.className = "left"
+    if (slideNum > 1) {
+        // Add a back button:
+        const btnBack = document.createElement("a")
+        btnBack.className = "btn btn-prev"
+        btnBack.innerHTML = '<i class="fa-solid fa-arrow-left"></i>Prev Page'
+        btnBack.href = "#slide" + padZeroes(slideNum-1)
+        divLeft.appendChild(btnBack)
+    }
+
     const divCenter = document.createElement("div")
     divCenter.className = "center"
+    divCenter.innerHTML = padZeroes(slideNum) + " / " + padZeroes(totalSlides)
+
     const divRight = document.createElement("div")
     divRight.className = "right"
-
-    divCenter.innerHTML = padZeroes(slideNum) + " / " + padZeroes(totalSlides)
+    if (slideNum < totalSlides-1) {
+        // Add a forward button:
+        const btnNext = document.createElement("a")
+        btnNext.className = "btn btn-next"
+        btnNext.innerHTML = 'Next Page<i class="fa-solid fa-arrow-right"></i>'
+        btnNext.href = "#slide" + padZeroes(slideNum+1)
+        divRight.appendChild(btnNext)
+    }
 
     footer.appendChild(divLeft)
     footer.appendChild(divCenter)
