7550009d45cf5ff1b84eaad4bbaec8243cea5dc1
agral <adam@gralin.ski>
2025-11-03 18:41:33 +0100

    Add functionality for inserting slide numbers
    This commit implements a different approach; one in which
    all the slides are processed once, at startup.
    This will finally result in a slide definition that will
    not require providing a footer at all.
    For the time being, it modifies the slide number part
    of the footer on each individual slide.

--- Web_presentation_template/main.js
+++ Web_presentation_template/main.js
@@ -5,7 +5,7 @@ function padZeroes(num, places) {
 }
 
 function modifyFooter() {
-    const slideId = "slide" + padZeroes(currSlide, 2)
+    let slideId = "slide" + padZeroes(currSlide, 2)
     let currSection = document.getElementById(slideId)
     if (currSection == null) {
         console.log("Error: could not find '" + slideId + "' which is the current slide.")
@@ -14,12 +14,34 @@ function modifyFooter() {
     }
     let currFooter = currSection.getElementsByTagName("footer")[0];
     if (currFooter == null) {
-        console.log("Error: could not find a footer in'" + slideId + "'.")
-        console.log("Please fix the syntax of slides in index.html.")
-        console.log("Aborting modifyFooter(), navigation will not work.")
         return
     }
     console.log("Successfully got hold of the current slide's footer.")
 }
-console.log("test")
-modifyFooter()
+
+function InsertSlideNumber(num, footer) {
+    let divCenter = footer.getElementsByClassName("center")[0]
+    if (divCenter == null) {
+        console.log("Error: could not find div.center in '" + footer + "'.")
+        console.log("Please fix the syntax of slides in index.html.")
+        return
+    }
+    divCenter.innerHTML = "testing" + num
+}
+
+function main() {
+    let slides = document.getElementsByClassName("slide")
+
+    for (let i = 0; i < slides.length; i++ ) {
+        let currFooter = slides[i].getElementsByTagName("footer")[0]
+        if (currFooter == null) {
+            console.log("Error: could not find a footer in slide#'" + (i+1) + "'.")
+            console.log("Please fix the syntax of slides in index.html.")
+            console.log("Aborting, navigation will not work.")
+            return
+        }
+        InsertSlideNumber(i+1, currFooter)
+    }
+}
+
+main()
